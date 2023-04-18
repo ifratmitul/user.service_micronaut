c26e49be7de92a575123a679206d1ddcf1b10545
ifratmitul <ifrat.ikhtear@gmail.com>
2023-04-18 17:55:51 +0600

    added manual migration with flyway

---
+++ src/main/resources/db/migration/V1__create-user-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+        `name` varchar(255) default null,
+        `email` varchar(255) default null,
+        `mobile_number` varchar(255) default null,
+         PRIMARY KEY (`id`)
+)
